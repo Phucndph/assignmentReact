ad15a9a3e5f97427b19f3dbbfd2548a9ac2aaff1
Phucndph <151540191+Phucndph@users.noreply.github.com>
2024-04-06 17:35:22 +0700

    Delete Demo51 directory

--- Demo51/actions.js
+++
@@ -1,3 +0,0 @@
-//Tao dau tien
-export const tang=()=>({type: 'TANG'});//khai bao ham tang
-export const giam=()=>({type: 'GIAM'});//khai bao ham giam
--- Demo51/store.js
+++
@@ -1,5 +0,0 @@
-//TAO THU 3
-import { createStore } from "redux";
-import demReducer from "./reducers";
-const store=createStore(demReducer);
-export default store;
